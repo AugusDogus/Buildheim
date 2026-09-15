bfd861b94443f982ed935c0d8cf7205ac1289645
AugusDogus <9794679+AugusDogus@users.noreply.github.com>
2026-09-15 00:39:29 -0500

    Release v1.2.1

--- src/Buildheim/Plugin.cs
+++ src/Buildheim/Plugin.cs
@@ -13,7 +13,7 @@ namespace PlanBuild
     {
         public const string PluginGUID = "augusdogus.Buildheim";
         public const string PluginName = "Buildheim";
-        public const string PluginVersion = "1.2.0";
+        public const string PluginVersion = "1.2.1";
 
         private ClientPlanner planner;
 
